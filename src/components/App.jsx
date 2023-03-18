eb8d14814894cecb25a5d84989891d3e3e6db5b4
SerhiiRodin <rodinserj@ukr.net>
2023-03-18 23:46:30 +0200

    add task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import transactions from './path/to/transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -18,7 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
